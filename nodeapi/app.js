c61df758b0b0f91f2af229892e057b6313295901
Aman Thukral <thukral618@gmail.com>
2020-08-15 06:13:29 +0530

    initializing project

---
+++ nodeapi/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// db
+// MONGO_URI=mongodb://localhost/nodeapi
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+    .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+// bring in routes
+const postRoutes = require("./routes/post");
+
+// middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use("/", postRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`A Node Js API is listening on port: ${port}`);
+});
---
+++ nodeapi/controllers/post.js
@@ -0,0 +1,18 @@
+const Post = require("../models/post");
+
+exports.getPosts = (req, res) => {
+    const posts = Post.find().select()
+        .then((posts) => {
+            res.json({ posts })
+        })
+        .catch(err => console.log(err));
+};
+
+exports.createPost = (req, res) => {
+    const post = new Post(req.body);
+    post.save().then(result => {
+        res.status(200).json({
+            post: result
+        });
+    });
+};
