45b58a2087db82c9f48ea6ee7571645ba4e096e1
Aman Thukral <thukral618@gmail.com>
2020-08-15 19:36:51 +0530

    adding signing in

--- nodeapi/routes/auth.js
+++ nodeapi/routes/auth.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { signup } = require("../controllers/auth");
-//const validator = require("../validator");
+const { userSignupValidator } = require("../validator");
 
 const router = express.Router();
 
 
-router.post("/signup", signup);
+router.post("/signup", userSignupValidator, signup);
 
 module.exports = router;
