4e138c61a583f131e45f186eb3fbff89d50dd681
Aman Thukral <thukral618@gmail.com>
2020-08-18 16:43:42 +0530

    adding update post functionality

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 const expressValidator = require("express-validator");
+const fs = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -24,6 +25,17 @@ mongoose.connection.on("error", err => {
 const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+app.get("/", (req, res) => {
+    fs.readFile('docs/apiDocs.json', (err, data) => {
+        if (err) {
+            res.status(400).json({
+                error: err
+            });
+        }
+        const docs = JSON.parse(data);
+        res.json(docs);
+    });
+});
 // middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const { getPosts, createPost, postByUser, postById, isPoster, deletePost } = require("../controllers/post");
+const { getPosts, createPost, postByUser, postById, isPoster, deletePost, updatePost } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { createPostValidator } = require("../validator");
 const { userById } = require("../controllers/user");
 
 const router = express.Router();
 
-router.get("/", getPosts);
+router.get("/posts", getPosts);
 router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
 
 router.get("/posts/by/:userId", requireSignin, postByUser);
 router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+router.put("/post/:postId", requireSignin, isPoster, updatePost);
 router.param("userId", userById);
 router.param("postId", postById);
 
