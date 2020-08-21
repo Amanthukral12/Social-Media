32b41930999f357ac04233dd77655fc1c15227e6
Aman Thukral <thukral618@gmail.com>
2020-08-22 04:44:55 +0530

    working on profile page

--- frontend/src/user/Profile.js
+++ frontend/src/user/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { isAuthenticated } from '../auth';
+import { Redirect } from 'react-router-dom';
 
 export default class Profile extends Component {
     constructor() {
@@ -11,14 +12,35 @@ export default class Profile extends Component {
     }
 
     componentDidMount() {
-        console.log("user id from the route params: ", this.props.match.params.userId)
+        const userId = this.props.match.params.userId
+        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${isAuthenticated().token}`
+            }
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                if (data.error) {
+                    this.setState({ redirectToSignin: true });
+                } else {
+                    this.setState({ user: data });
+                }
+            });
     }
     render() {
+        const redirectToSignin = this.state.redirectToSignin
+        if (redirectToSignin) return <Redirect to="/signin" />
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Profile</h2>
                 <p>Hello {isAuthenticated().user.name}</p>
                 <p>Email: {isAuthenticated().user.email}</p>
+                <p>{`Joined ${new Date(this.state.user.created).toDateString()}`}</p>
             </div>
         )
     }
