89bb12963951aa894f3b698dd0a5e356e51e3d5c
Aman Thukral <thukral618@gmail.com>
2020-08-24 02:09:38 +0530

    adding default profile picture

--- frontend/src/user/Users.js
+++ frontend/src/user/Users.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { list } from './apiUser';
+import DefaultProfile from '../images/avatar.png';
 export default class Users extends Component {
     constructor() {
         super()
@@ -21,7 +22,7 @@ export default class Users extends Component {
         <div className="row">
             {users.map((user, i) => (
                 <div className="card col-md-4" key={i}>
-                    <img className="card-img-top" src="" alt="Card image cap" />
+                    <img className="card-img-top" src={DefaultProfile} alt="Card image cap" style={{ width: '100%', height: '15vw', objectFit: 'cover' }} />
                     <div className="card-body">
                         <h5 className="card-title">{user.name}</h5>
                         <p className="card-text">{user.email}</p>
