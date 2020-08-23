d3a2dfd307991a0468091427b7fafaf8cce4e725
Aman Thukral <thukral618@gmail.com>
2020-08-24 02:34:49 +0530

    addingsome functionality

--- frontend/src/user/Profile.js
+++ frontend/src/user/Profile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { isAuthenticated } from '../auth';
 import { Redirect, Link } from 'react-router-dom';
 import { read } from "./apiUser";
+import DefaultProfile from '../images/avatar.png';
 
 export default class Profile extends Component {
     constructor() {
@@ -33,16 +34,22 @@ export default class Profile extends Component {
         if (redirectToSignin) return <Redirect to="/signin" />
         return (
             <div className="container">
+                <h2 className="mt-5 mb-5">Profile</h2>
                 <div className="row">
+
                     <div className="col-md-6">
-                        <h2 className="mt-5 mb-5">Profile</h2>
-                        <p>Hello {isAuthenticated().user.name}</p>
-                        <p>Email: {isAuthenticated().user.email}</p>
-                        <p>{`Joined ${new Date(user.created).toDateString()}`}</p>
+
+                        <img className="card-img-top" src={DefaultProfile} alt="Card image cap" style={{ width: '50%', height: '15vw', objectFit: 'cover', borderRadius: '100%' }} />
+
                     </div>
                     <div className="col-md-6">
+                        <div className="lead mt-2">
+                            <p>Hello {user.name}</p>
+                            <p>Email: {user.email}</p>
+                            <p>{`Joined ${new Date(user.created).toDateString()}`}</p>
+                        </div>
                         {isAuthenticated().user && isAuthenticated().user._id === user._id && (
-                            <div className="d-inline-block mt-5">
+                            <div className="d-inline-block">
                                 <Link className="btn btn-raised btn-success mr-5" to={`/user/edit/${user._id}`}>
                                     Edit Profile
                         </Link>
--- frontend/src/user/Users.js
+++ frontend/src/user/Users.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { list } from './apiUser';
+import { Link } from 'react-router-dom';
 import DefaultProfile from '../images/avatar.png';
 export default class Users extends Component {
     constructor() {
@@ -22,11 +23,11 @@ export default class Users extends Component {
         <div className="row">
             {users.map((user, i) => (
                 <div className="card col-md-4" key={i}>
-                    <img className="card-img-top" src={DefaultProfile} alt="Card image cap" style={{ width: '100%', height: '15vw', objectFit: 'cover' }} />
+                    <img className="card-img-top" src={DefaultProfile} alt="Card image cap" style={{ width: '50%', height: '15vw', objectFit: 'cover', borderRadius: '100%' }} />
                     <div className="card-body">
                         <h5 className="card-title">{user.name}</h5>
                         <p className="card-text">{user.email}</p>
-                        <a href="#" className="btn btn-raised btn-primary btn-sm">View Profile</a>
+                        <Link to={`user/${user._id}`} className="btn btn-raised btn-primary btn-sm">View Profile</Link>
                     </div>
                 </div>
             ))}
