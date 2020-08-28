710d40038d2884a4a543e710836270e959b630f7
Aman Thukral <thukral618@gmail.com>
2020-08-28 20:37:23 +0530

    editing the database for profile image upload

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -1,69 +1,85 @@
-const _ = require('lodash');
+const _ = require("lodash");
 const User = require("../models/user");
+const formidable = require("formidable");
+const fs = require("fs");
 
 exports.userById = (req, res, next, id) => {
-    User.findById(id).exec((err, user) => {
-        if (err || !user) {
-            return res.status(400).json({
-                error: "User not found"
-            })
-        }
-        req.profile = user;
-        next();
-    });
+  User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+    req.profile = user;
+    next();
+  });
 };
 
 exports.hasAuthorization = (req, res, next) => {
-    const authorized = req.profile && req.auth && req.profile._id === req.auth._id
-    if (!authorized) {
-        return res.status(403).json({
-            error: "User is not authorized to perform this action"
-        });
-    };
+  const authorized =
+    req.profile && req.auth && req.profile._id === req.auth._id;
+  if (!authorized) {
+    return res.status(403).json({
+      error: "User is not authorized to perform this action",
+    });
+  }
 };
 
-
 exports.allUsers = (req, res) => {
-    User.find((err, users) => {
-        if (err) {
-            return res.status(400).json({
-                error: err
-            })
-        }
-        res.json(users);
-    }).select("name email updated created");
-}
+  User.find((err, users) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json(users);
+  }).select("name email updated created");
+};
 
 exports.getUser = (req, res) => {
-    req.profile.hashed_password = undefined;
-    req.profile.salt = undefined;
-    return res.json(req.profile);
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
 };
 
 exports.updateUser = (req, res, next) => {
-    let user = req.profile
-    user = _.extend(user, req.body)
-    user.updated = Date.now()
-    user.save((err) => {
-        if (err) {
-            return res.status(400).json({
-                error: "You are not authorised"
-            });
-        }
-        req.profile.hashed_password = undefined;
-        req.profile.salt = undefined;
-        res.json({ user })
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Photo could not be uploaded",
+      });
+    }
+    let user = req.profile;
+    user = _.extend(user, fields);
+    user.updated = Date.now();
+
+    if (files.photo) {
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contenType = files.photo.type;
+    }
+    user.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json(user);
     });
+  });
 };
 
 exports.deleteUser = (req, res, next) => {
-    let user = req.profile
-    user.remove((err, user) => {
-        if (err) {
-            return res.status(400).json({
-                error: err
-            })
-        }
-        res.json({ message: "User deleted" });
-    })
-}
+  let user = req.profile;
+  user.remove((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json({ message: "User deleted" });
+  });
+};
--- nodeapi/models/user.js
+++ nodeapi/models/user.js
@@ -1,54 +1,60 @@
-const mongoose = require('mongoose');
-const { v1: uuidv1 } = require('uuid');
-const crypto = require('crypto');
+const mongoose = require("mongoose");
+const { v1: uuidv1 } = require("uuid");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    hashed_password: {
-        type: String,
-        required: true
-    },
-    salt: String,
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    updated: Date
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  hashed_password: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: Date,
+  photo: {
+    data: Buffer,
+    contenType: String,
+  },
 });
 
-userSchema.virtual('password')
-    .set(function (password) {
-        this._password = password
-        this.salt = uuidv1()
-        this.hashed_password = this.encryptPassword(password)
-    })
-    .get(function () {
-        return this._password
-    })
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 userSchema.methods = {
-    authenticate: function (plainText) {
-        return this.encryptPassword(plainText) === this.hashed_password
-    },
-    encryptPassword: function (password) {
-        if (!password) return "";
-        try {
-            return crypto.createHmac('sha1', this.salt)
-                .update(password)
-                .digest('hex');
-        } catch (err) {
-            return "";
-        }
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
     }
-}
+  },
+};
 
 module.exports = mongoose.model("User", userSchema);
